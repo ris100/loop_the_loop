defac76a1b2ad0f66162a9ae66e6869100003e87
Rishabh Sinha <ris100@RAHULs-Air.home>
2016-02-14 12:58:12 -0500

    all pgms

---
+++ Rishabhruby.rb
@@ -0,0 +1,15 @@
+puts "Holy giraffes! You fell into a maze!"
+print "Where to? (N,E,S,W):"
+direction = gets.chomp
+puts "#{direction},you say? A fine choice!"
+if direction =="N"
+	puts "you are in a maze with twisty little passages,all alike."
+elsif direction =="E"
+		puts "An elf and his pet Ham!"
+elsif direction =="S"
+  puts "A minotaur! Wait,no,thats your reflection."
+elsif direction == "W"
+  puts "You're here wherever here is."
+else
+  puts "Wait, is that even a direction?"
+end        
