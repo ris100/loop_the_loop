c7a2db6488314dc6db189a37f97ab1f3c8a759b5
Rishabh Sinha <ris100@RAHULs-Air.home>
2016-02-15 18:28:16 -0500

    "[GIT AUTO COMMIT]: Mon Feb 15 2016 18:28:16 GMT-0500 (EST)"

--- node/stuff.rb
+++ node/stuff.rb
@@ -1,5 +1,14 @@
 class Car
   attr_accessor :name,:color,:model,:company
+  def start()
+    puts "#{name} started"
+  end  
+  def stop()
+    puts "#{name} stop"
+  end  
+end
+class KukkuCar < Car
+  
 end
 hondaaccord=Car.new
 hondaaccord.color="blue"
