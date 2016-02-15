7a720254295fe1eb443ee58933d149c1f232d522
Rishabh Sinha <ris100@RAHULs-Air.home>
2016-02-15 18:40:34 -0500

    "[GIT AUTO COMMIT]: Mon Feb 15 2016 18:40:34 GMT-0500 (EST)"

--- flowmatic_rules.rb
+++ flowmatic_rules.rb
@@ -11,4 +11,4 @@ if flow_rate > 50
   flow_rate = 50
   puts "The flow_rate's been reset to #{flow_rate}."
 elsif flow_rate < 50
-  puts "Warning the flow_rate is under 50"              
+  puts "Warning the flow_rate is under 50! It's"              
