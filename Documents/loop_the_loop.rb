d095128601ca9f426dd5f93ed2d7297090c67c41
Rishabh Sinha <ris100@RAHULs-Air.home>
2016-02-15 17:42:52 -0500

    "[GIT AUTO COMMIT]: Mon Feb 15 2016 17:42:52 GMT-0500 (EST)"

---
+++ Documents/loop_the_loop.rb
@@ -0,0 +1,16 @@
+we_wanna_ride =true
+stops = ["fellswood drive","ymca","libary","penag","barsen nobles","wushu"]
+while we_wanna_ride 
+  print "Where we headin',friend?:"
+  destination = gets.chomp
+  if stops.include? destination
+  puts "I know how to get to #{destination}! Here's the station list:"                   
+  stops.each do |stop|
+    puts stop
+    break if stop == destination
+  end  
+ else 
+  puts "Sorry,we don't stop at that station. Maybe another time!"
+  we_wanna_ride = false
+ end 
+end 
