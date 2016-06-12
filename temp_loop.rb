511193bbf03ddc37c6ffc2cea94ce6c4d6716dba
Rishabh Sinha <ris100@RAHULs-Air.home>
2016-06-11 20:21:21 -0400

    "[GIT AUTO COMMIT]: Sat Jun 11 2016 20:21:20 GMT-0400 (EDT)"

---
+++ temp_loop.rb
@@ -0,0 +1,11 @@
+# This program will keep asking user to enter the temperature in
+#farhenheit and it will convert to celcius 
+#It will terminate when user types temp found  
+we_wanna_find_temparature=true
+while we_wanna_find_temparature
+  print "whats the temparature in fahrenheit?"
+  f=gets.chomp.to_i
+   celcius = (f - 32) * 5 / 9
+   puts "Here is the temparature in celcius #{celcius}"
+end
+  
